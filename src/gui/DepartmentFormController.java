03154b37ed7de1ddcce25ec81cd3beeffd07a426
GabrielSan99 <gabriel_j.sanches@hotmail.com>
2020-05-10 10:37:32 -0300

    Observer design pattern to update tableview

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
@@ -22,6 +25,8 @@ public class DepartmentFormController implements Initializable {
 	private Department entity; // entity é privada e tem seu set para chamada
 	
 	private DepartmentService service;
+	
+	private List<DataChangeListener> dataChangeListener = new ArrayList<>();
 	
 	@FXML
 	private TextField txtId;
@@ -46,6 +51,10 @@ public class DepartmentFormController implements Initializable {
 		this.service = service;
 	}
 	
+	public void subscribeDataChageListener(DataChangeListener listener) {
+		dataChangeListener.add(listener);
+		}
+	
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
 		if (entity == null) {
@@ -57,6 +66,7 @@ public class DepartmentFormController implements Initializable {
 		try {
 			entity = getFormData(); 
 			service.saveOrUpdate(entity); // aqui vai salvar no banco de dados
+			notifyDataChangeListeners();
 			Utils.currentStage(event).close(); // quando o botão for apertado ele fecha a janela e salva
 			
 		}
@@ -65,6 +75,13 @@ public class DepartmentFormController implements Initializable {
 		}
 	}
 	
+	private void notifyDataChangeListeners() {
+		for(DataChangeListener listener : dataChangeListener) {
+			listener.onDataChange();
+		}
+		
+	}
+
 	private Department getFormData() { // responsavel por pegar os dados digitados no formulario e instaciar no departamento
 		Department obj = new Department();
 		
