deaadd64223a40626ba55722992889c0ebcfcb76
GabrielSan99 <gabriel_j.sanches@hotmail.com>
2020-05-11 22:55:53 -0300

    SellerForm

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,141 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exceptions.ValidationException;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable {
+	
+	private Seller entity; // entity é privada e tem seu set para chamada
+	
+	private SellerService service;
+	
+	private List<DataChangeListener> dataChangeListener = new ArrayList<>();
+	
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private Label labelErrorName;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	public void setSeller(Seller entity) { // set do entity
+		this.entity = entity;
+	}
+	
+	public void setSellerSerrvice(SellerService service) {
+		this.service = service;
+	}
+	
+	public void subscribeDataChageListener(DataChangeListener listener) {
+		dataChangeListener.add(listener);
+		}
+	
+	@FXML
+	public void onBtSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entity = getFormData(); 
+			service.saveOrUpdate(entity); // aqui vai salvar no banco de dados
+			notifyDataChangeListeners();
+			Utils.currentStage(event).close(); // quando o botão for apertado ele fecha a janela e salva
+			
+		}
+		catch (ValidationException e) {
+			setErrorMessages(e.getErrors()); // toda a programação dos erros la embaixo estão em função de clicar no botão save
+		}
+		catch (DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private void notifyDataChangeListeners() {
+		for(DataChangeListener listener : dataChangeListener) {
+			listener.onDataChange();
+		}
+		
+	}
+
+	private Seller getFormData() { // responsavel por pegar os dados digitados no formulario e instaciar no departamento
+		Seller obj = new Seller();
+		
+		ValidationException exception = new ValidationException("Validation Error"); // instanciando a excessaõ
+		
+		obj.setId(Utils.tryParseToInt (txtId.getText())); // o método de conversão presente no utils
+		
+		if (txtName.getText() == null || txtName.getText().trim().equals ("")) {
+			exception.addError("name", "Field can't be empty"); // isso faz armazenar no map do validation exception e imprimir a mensagem de erro
+		}
+		obj.setName(txtName.getText());
+		
+		if (exception.getErrors().size()> 0) { // ele vai ver o map e vai imprimir a excessão
+			throw exception;
+		}
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close(); // quando o botão for apertado ele fecha a janela e salva
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		initializeNodes();
+	}
+	
+	private void initializeNodes() { // privações no preenchimento
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+
+	public void updateFormData() {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		else {
+		txtId.setText(String.valueOf(entity.getId())); // aqui é onde ocorrem as mudanças quando  utilizado  os textbox
+		txtName.setText(entity.getName());
+		}
+	}
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if (fields.contains("name")) { // se o erro "name" estiver presente ele vai imprimir a msg de erro na label
+			labelErrorName.setText(errors.get("name")); // vai pegar a msg atrelada a "name" no map e escrever na label
+		}
+	}
+}
--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -16,7 +17,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -24,6 +27,8 @@ import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.services.SellerService;
@@ -109,33 +114,33 @@ public class SellerListController implements Initializable, DataChangeListener {
 	}
 
 	private void createDialogForm(Seller obj, String absolutName, Stage parentStage) { // chamada da tela para
-//																							// inserir um novo
-//																							// departamento, semelhante
-//																							// ao que existe no
-//																							// mainViewController
-//		try {
-//			FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutName));
-//			Pane pane = loader.load();
-//
-//			SellerFormController controller = loader.getController();
-//			controller.setSeller(obj);
-//			controller.setSellerSerrvice(new SellerService());
-//			controller.subscribeDataChageListener(this); // quando for executado ele conta essa tela para executar um
-//															// método de fechamento e abertura da tela departmentList
-//			controller.updateFormData();
-//
-//			Stage dialogStage = new Stage();
-//			dialogStage.setTitle("Enter Seller data");
-//			dialogStage.setScene(new Scene(pane));
-//			dialogStage.setResizable(false);
-//			dialogStage.initOwner(parentStage);
-//			dialogStage.initModality(Modality.WINDOW_MODAL); // criar o tipo de janela windows que não permite que vc
-//																// acesse a de baixo sem terminar com ela
-//			dialogStage.showAndWait();
-//
-//		} catch (IOException e) {
-//			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-//		}
+																							// inserir um novo
+																							// departamento, semelhante
+																							// ao que existe no
+																							// mainViewController
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutName));
+			Pane pane = loader.load();
+
+			SellerFormController controller = loader.getController();
+			controller.setSeller(obj);
+			controller.setSellerSerrvice(new SellerService());
+			controller.subscribeDataChageListener(this); // quando for executado ele conta essa tela para executar um
+															// método de fechamento e abertura da tela departmentList
+			controller.updateFormData();
+
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Seller data");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL); // criar o tipo de janela windows que não permite que vc
+																// acesse a de baixo sem terminar com ela
+			dialogStage.showAndWait();
+
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 	@Override
